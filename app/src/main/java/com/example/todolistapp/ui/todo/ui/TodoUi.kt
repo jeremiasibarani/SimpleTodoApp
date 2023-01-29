b2be7ba959038ef1d01a4ccdcc6a3e8c33cd115e
Jeremia Sibarani <sibaranigreamjeremia@gmail.com>
2023-01-29 21:06:15 +0700

    add please wait dialog

--- app/src/main/java/com/example/todolistapp/ui/todo/ui/TodoUi.kt
+++ app/src/main/java/com/example/todolistapp/ui/todo/ui/TodoUi.kt
@@ -15,8 +15,11 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.todolistapp.database.TodoEntity
 import com.example.todolistapp.ui.theme.TodoListAppTheme
 import com.example.todolistapp.viewmodel.TodoViewModel
@@ -247,10 +250,65 @@ private fun CardTodo(
     }
 }
 
+@Composable
+fun CustomProgressBarDialog(
+    onDismiss : () -> Unit,
+    dismissOnBackPress : Boolean = false,
+    dismissOnClickOutside : Boolean = false,
+    modifier : Modifier = Modifier
+){
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(
+            dismissOnBackPress = dismissOnBackPress,
+            dismissOnClickOutside = dismissOnClickOutside
+        )
+    ) {
+        Card(
+            modifier = modifier,
+            shape = RoundedCornerShape(6)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(bottom = 20.dp),
+                    strokeWidth = 5.dp,)
+                Text(
+                    text = "Please wait..",
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
 
 
-//@Preview(showBackground = true)
-//@Composable
-//fun Prev() {
-//    TodoScreen()
-//}
+
+@Preview(showBackground = true)
+@Composable
+fun Prev() {
+    MaterialTheme() {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            CustomProgressBarDialog(
+                onDismiss = {  },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(10.dp)
+            )
+        }
+    }
+}
